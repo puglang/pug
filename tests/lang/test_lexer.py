12a861109ae91cc4e571c71554e7216a441e1ed7
Johnny Starr <johnnystarr@gmail.com>
2022-05-26 11:38:35 -0500

    custom lexical error, adding basic methods

--- tests/lang/test_lexer.py
+++ tests/lang/test_lexer.py
@@ -1,6 +1,6 @@
 import pytest
 
-from puglang.lang.lexer import Lexer
+from puglang.lang.lexer import Lexer, LexicalError
 
 
 @pytest.fixture
@@ -10,3 +10,17 @@ def lexer():
 
 def test_lexer_is_not_none(lexer):
     assert lexer is not None
+
+
+def test_lexer_load_source_code(lexer):
+    lexer.load("i=1")
+
+
+def test_lexer_fails_without_load(lexer):
+    with pytest.raises(LexicalError):
+        lexer.lex()
+
+
+def test_lexer_loads_and_lex(lexer):
+    lexer.load("i=1")
+    lexer.lex()
